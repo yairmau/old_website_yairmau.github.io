233df75f0614cb6ce1fed35e5b4f6ad4601015f1
Yair Mau <yairmau@gmail.com>
2017-04-20 16:02:13 +0300

    deploy script added

--- setup.sh
+++ setup.sh
@@ -30,4 +30,4 @@ git checkout $SOURCE
 
 msg "Adding the master branch into the \`public\` folder"
 rm -rf public
-git worktree -B master public origin/master
+git worktree add -B master public origin/master
